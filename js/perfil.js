8ee043e29b7669340f7d7c7ffe0eea42f5c0e728
SebaTrujillo94 <s.trijillo@duocuc.cl>
2025-03-31 11:45:41 -0300

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     });
 
     if (valido) {
-      mostrarMensajeExito("✅ Cambios guardados correctamente.");
+      mostrarMensajeExito("✅ Cambios guardados.");
 
       setTimeout(() => {
         $("#perfilForm")[0].reset(); // Limpiar campos
